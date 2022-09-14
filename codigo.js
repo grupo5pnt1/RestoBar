04b3541ecadb724678f71fbd181c9a86953ec648
grupo5pnt1 <111472009+grupo5pnt1@users.noreply.github.com>
2022-09-14 20:50:18 -0300

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -1,9 +1,30 @@
 function capturar () {
-    var pe = document.getElementById("pocillo").value;
-    alert("Tu orden es la siguiente: " 
-    + "Pocillo " + pe
-    
-    
-    );
+    var nom = document.getElementById('name').value;
+    var fecha = document.getElementById('start').value;
+    var email = document.getElementById('email').value;
+    if (nom !=''){
+     if (fecha !='')
+     {
+        if (email !=''){
+
+            alert("Su orden ha sido registrada! Muchas gracias por tu compra " + nom);
+
+            } else {
+              alert("Falta el campo Email");
+             document.getElementById('email').focus();
+
+            }
+          
+
+     } else {
+        alert("Falta el campo Fecha");
+		document.getElementById('start').focus();
+     }
+    } else 
+    {
+        alert("Falta el campo Nombre");
+		document.getElementById('name').focus();
+    }
+
 
 }
